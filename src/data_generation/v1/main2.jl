8e32b18daae4f04f99eac365ffe95b4e90b73696
GoodStuff11 <uikluspe@gmail.com>
2023-12-20 16:51:02 -0500

    changes

--- src/data_generation/v1/main2.jl
+++ src/data_generation/v1/main2.jl
@@ -5,13 +5,13 @@ using CSV
 using Images
 
 function main(input)
-    for p = 0.01:0.01:0.99
+    for p = 0.01:0.001:0.99
         input["porosity"] = p
         blocked, percolation_probability = compute_percolation_probability(input)
         path = "C:\\Users\\jonat\\OneDrive\\Documents\\programming\\AnacondaProjects\\PHYS495\\final\\data\\perco_prob\\"
         data = permutedims(repeat(blocked*1.0, inner=[1,1,3]),[3,1,2])
         image = colorview(RGB, data)
-        save(string(path,round(1-p,),"_", percolation_probability,".png"), image)
+        save(string(path,round(1-p;digits=3),"_", percolation_probability,".png"), image)
     end
 end
 
